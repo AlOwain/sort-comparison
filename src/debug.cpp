7f814692c53fe66820a75c4907423581a97eac8b
AlOwain <abdulaziz@alowain.dev>
2024-01-14 18:18:55 +0300

    Implemented `insertion_sort`
    this commit implements insertion sort and provides a `print(std::vector)` function that outputs the given vector to the standard output.

--- src/debug.cpp
+++ src/debug.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <vector>
+
+std::vector<int> insertion_sort(std::vector<int> array)
+{
+    int max, j = 0;
+    for (int i = 1; i < array.size(); i++) {
+        max = array[i];
+
+        for (j = i - 1; j >= 0; j--)
+            if (array[j] > max) break;
+            else array[j + 1] = array[j];
+        array[j + 1] = max;
+    }
+    return array;
+}
+
+void print(std::vector<int> array)
+{
+    for (int i : array)
+        std::cout << i << " ";
+    std::cout << std::endl;
+}
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello World\n";
+    std::vector<int> test = {1, 12, 9, 0, 5, 3};
+
+    print(insertion_sort(test));
 
     return 0;
 }
